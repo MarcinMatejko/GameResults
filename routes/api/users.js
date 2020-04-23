7e49ef4909e3dd16083e0907996cfe6f03509d97
Marcin Matejko <matejko.tsp@gmail.com>
2020-04-23 11:24:39 +0200

    validacja uzytkownika

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/users
-// @desc    Test Route
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Imię jest wymagane').not().isEmpty(),
+    check('email', 'Proszę podać prawidłowy adres email').isEmail(),
+    check('password', 'Hasło musi zawierać minimum 6 znaków').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
