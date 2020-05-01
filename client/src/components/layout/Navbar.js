2e806eee703ed6a65116d5224b766f67414413f5
Marcin Matejko <matejko.tsp@gmail.com>
2020-05-01 16:19:40 +0200

    dodano funcje wyloguj

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const Navbar = () => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <a onClick={logout} href='#!'>
+          Wyloguj
+        </a>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to='/register'>Zarejestruj</Link>
+      </li>
+      <li>
+        <Link to='/login'>Zaloguj</Link>
+      </li>
+    </ul>
+  );
   return (
     <nav className='navbar'>
       <p>
         <Link to='/'>Punktator</Link>
       </p>
-      <ul>
-        <li>
-          <Link to='/register'>Zarejestruj</Link>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <Link to='/login'>Zaloguj</Link>
-        </li>
-      </ul>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
